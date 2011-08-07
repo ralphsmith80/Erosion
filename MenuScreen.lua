6e7942043c6501d5add53e0086ac69327faf1f1a
Ralph Smith <ralphsmith80@gmail.com>
2011-08-07 16:19:53 -0500

    added redBtn.png and pressedRedBtn.png for better physical shape for testing and development

--- MenuScreen.lua
+++ MenuScreen.lua
@@ -7,20 +7,10 @@ MenuScreen = Screen:new("menuScreen")
 
 --local imageGroup = display.newGroup()
 local background
-local newGameBtn
 local playBtn
 local howToBtn
 local highScoreBtn
 local state
-
-local newGameBtnPress = function( event )
-	--print("pressed newGameBtn")
-end
-
-local newGameBtnRelease = function( event )
-	--print("released newGameBtn")
-	state = "gameScreen"
-end
 
 local playBtnRelease = function( event )
 	state = "gameScreen"
@@ -41,43 +31,37 @@ function MenuScreen:init ()
 	--print("init menu screen")
 	background = display.newImage("menuBackground.png")
 	--imageGroup:insert (splashImage)
---[[	newGameBtn = ui.newButton{
-		default = "newGameBtn.png",
-		over = "quitGameBtn.png",
-		onPress = newGameBtnPress,
-		onRelease = newGameBtnRelease,
-		id = "newGameBtn"
-		--emboss = true
-	}
-]]
 	playBtn = ui.newButton{
-		default = "newGameBtn.png",
-		over = "quitGameBtn.png",
+		default = "redBtn.png",
+		over = "pressedRedBtn.png",
 		--onPress = newGameBtnPress,
 		onRelease = playBtnRelease,
+		text = "Play Game",
 		id = "playBtn"
 		--emboss = true
 	}
 	howToBtn = ui.newButton{
-		default = "newGameBtn.png",
-		over = "quitGameBtn.png",
+		default = "redBtn.png",
+		over = "pressedRedBtn.png",
 		--onPress = newGameBtnPress,
 		onRelease = howToBtnRelease,
+		text = "How-To",
 		id = "howToBtn"
 		--emboss = true
 	}
 	highScoreBtn = ui.newButton{
-		default = "newGameBtn.png",
-		over = "quitGameBtn.png",
+		default = "redBtn.png",
+		over = "pressedRedBtn.png",
 		--onPress = newGameBtnPress,
 		onRelease = highScoreBtnRelease,
+		text = "High Scores",
 		id = "highScoreBtn"
 		--emboss = true
 	}
---	newGameBtn.x = 160; newGameBtn.y=240
-	playBtn.x = 200; playBtn.y = 240
-	howToBtn.x = 100; howToBtn.y = 300
-	highScoreBtn.x = 200; highScoreBtn.y = 360
+
+	playBtn.x = 240; playBtn.y = 220
+	howToBtn.x = 80; howToBtn.y = 300
+	highScoreBtn.x = 240; highScoreBtn.y = 380
 	state = self.state
 end
 
@@ -101,7 +85,6 @@ end
 function MenuScreen:exit ()
 	--print("exit menu screen")
 	background.parent:remove( background )
-	--newGameBtn.parent:remove( newGameBtn )
 	playBtn.parent:remove( playBtn )
 	howToBtn.parent:remove( howToBtn )
 	highScoreBtn.parent:remove( highScoreBtn )
